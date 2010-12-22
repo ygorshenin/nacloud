721fc9bf63f7f670dbc13471aa0d3be9a3914228
ygorshenin <ygorshenin@google.com>
2010-12-22 16:13:16 +0300

    Added job verification code

--- prototype/allocator/job_runner.rb
+++ prototype/allocator/job_runner.rb
@@ -26,12 +26,16 @@ require 'lib/package/spm'
 require 'optparse'
 
 ACTIONS = [:up, :down]
+IDENTIFIER_REGEX = /^[a-zA-Z0-9_]+$/
 
 def get_db_client(server, host)
   port = server.get_db_client_port
   DRbObject.new_with_uri("druby://#{host}:#{port}")
 end
 
+# Parses script options from argv.
+# Raises ArgumentError, if important options are missed.
+# Returns parsed options as Hash.
 def parse_options(argv)
   parser, options = OptionParser.new, { :action => :up }
 
@@ -46,31 +50,214 @@ def parse_options(argv)
   [:config, :host, :port, :user].each do |option|
     raise ArgumentError.new("option '#{option}' must be specified") unless options[option]
   end
+
+  if options[:user] !~ IDENTIFIER_REGEX
+    raise ArgumentError.new("option 'user' must be a standart identifier")
+  end
+  
+  # Ruby doesn't understand ~ abbrevs, so it's better to manually expand path
   options[:config] = File.expand_path(options[:config])
   options
 end
 
-# Returns true if success
-def upload_job(server, db_client, packages, job_options)
-  begin
-    return false if db_client.exists_job?(job_options)
-    
-    db_client.insert_job(job_options)
+class ConfigUtils
+  # Reads and prepares configuration file from options[:config]
+  def self.read_config(options)
+    prepare_config(get_options_from_file(options[:config]), options)
+  end
+  
+  # Prepares jobs configuration file.
+  # Modifies config and returns it.
+  # Options are not modified.
+  def self.prepare_config(config, options = {})
+    # There always must be :jobs and :packages keys
+    config[:jobs] = Array(config[:jobs] || [])
+    prepare_jobs(config[:jobs], options)
+    config[:packages] = Array(config[:packages] || [])
+    prepare_packages(config[:packages], options)
+    config
+  end
 
-    if job_options.has_key?(:binary)
-      File.open(job_options[:binary], 'r') { |file| db_client.insert_binary(file.read, job_options) }
+  private
+
+  # Prepares one package description.
+  # Modifies argument and returns it.
+  def self.prepare_package(package, options = {})
+    package.symbolize_keys_recursive! # Symbolize all keys in package description
+    package[:name] = String(package[:name]) if package.has_key? :name
+    package[:data] = Array(package[:data] || []) # Makes :data value array
+    package[:data].each { |h| h.symbolize_keys_recursive! } # Symbolize all :data keys
+    package[:data].each do |h|
+      if h.has_key? :source
+        h[:source] = File.expand_path(String(h[:source]))
+      end
+      h[:target] = String(h[:target]) if h.has_key? :target
+    end
+    package
+  end
+
+  # Prepares packages description.
+  # Packages must be an array of descriptions.
+  # Modifies argument and returns it.
+  def self.prepare_packages(packages, options = {})
+    packages.each { |package| prepare_package(package, options) }
+    packages
+  end
+
+  # Prepares job options.
+  # Modifies argument and returns it.
+  def self.prepare_job(job, options = {})
+    job.symbolize_keys_recursive! # Symbolize all keys in that Hash
+    job[:name] = String(job[:name]) if job.has_key? :name
+    job[:user] = String(job[:user] || options[:user]) # If nothing are presented (nil), empty string
+    job[:packages] = Array(job[:packages] || []) # job[:packages] must be array
+    job[:replicas] ||= 1
+    job[:options] ||= {} # job[:options] always must be
+    job[:options][:different_nodes] ||= false # By default different nodes is not necessary
+
+    if job.has_key? :binary
+      job[:binary] = File.expand_path(String(job[:binary]))
+    end
+
+    job[:command] = String(job[:command]) if job.has_key? :command
+  end
+
+  # Prepares jobs description.
+  # Jobs must be an array of descriptions.
+  # Modifies argument and returns it.
+  def self.prepare_jobs(jobs, options = {})
+    jobs.each { |job| prepare_job(job, options) }
+    jobs
+  end
+end
+
+module JobChecks
+  # Only checks job name.
+  # Raises exception, if fails.
+  def check_job_name(job)
+    raise ArgumentError.new('job must have a name') unless job.has_key? :name
+    raise ArgumentError.new("for job '#{job[:name]}': name must be an standart identifier") unless job[:name] =~ IDENTIFIER_REGEX
+  end
+
+  # Checks that all packages used by this job are exists.
+  # Raises exception, if fails.
+  def check_job_packages(job, packages)
+    job[:packages].each do |package|
+      raise ArgumentError.new("for job '#{job[:name]}': package #{package} not found") unless packages.has_key? package
+    end
+  end
+
+  # Checks job's description for consistensy.
+  # Doesn't checks job's dependencies.
+  # Raises exception, if fails.
+  def check_job(job)
+    check_job_name(job)
+    
+    if not job[:replicas].is_a?(Fixnum) or job[:replicas] < 1
+      raise ArgumentError.new("for job '#{job[:name]}': replicas must be a fixnum >= 1")
     end
     
-    if job_options.has_key?(:packages)
-      job_options[:packages].each do |package_name|
-        SPM::build(packages[package_name])
-        File.open(package_name, 'r') { |file| db_client.insert_package(file.read, { :package_name => package_name }.merge(job_options)) }
-        FileUtils.rm(package_name)
+    different_nodes = job[:options][:different_nodes]
+    if not different_nodes.is_a?(TrueClass) and not different_nodes.is_a?(FalseClass)
+      raise ArgumentError.new("for job '#{job[:name]}': different_nodes option must be a bool value")
+    end
+
+    if job.has_key?(:binary) and not File::exists?(job[:binary])
+      raise ArgumentError.new("for job'#{job[:name]}': binary '#{job[:binary]}' doesn't exists")
+    end
+  end
+
+  # Checks jobs descriptions for consistensy.
+  # Raises exception, if fails.
+  def check_jobs(jobs)
+    job_names = Set.new
+    jobs.each do |job|
+      check_job job
+      raise ArgumentError.new("duplicate job name: '#{job[:name]}'") if job_names.include? job[:name]
+      job_names.add job[:name]
+    end
+  end
+
+  # Checks package's description for consistency.
+  # Raises exception, if fails.
+  def check_package(package)
+    raise ArgumentError.new("package must have a name") unless package.has_key? :name
+    raise ArgumentError.new("for package '#{package[:name]}': name must be an standart identifier") unless package[:name] =~ IDENTIFIER_REGEX
+    package[:data].each do |h|
+      if h.has_key? :source
+        raise ArgumentError.new("for package '#{package[:name]}': source '#{h[:source]}' doesn't exists") unless File::exists? h[:source]
+        raise ArgumentError.new("for package '#{package[:name]}': for source  '#{h[:source]}' there must be an target") unless h.has_key? :target
       end
     end
+  end
+
+  # Checks packages descriptions for consistency.
+  # Raises exception, if fails.
+  def check_packages(packages)
+    package_names = Set.new
+    packages.each do |package|
+      check_package package
+      raise ArgumentError.new("duplicate package name: '#{package[:name]}'") if package_names.include? package[:name]
+      package_names.add package[:name]
+    end
+  end
+
+  def check_dependencies(jobs, packages)
+    h = {}
+    packages.each { |package| h[package[:name]] = package }
+    jobs.each { |job| check_job_packages(job, h) }
+  end
+end
+
+class StrongConfigChecker
+  # Checks configuration file.
+  # Checks all info.
+  # Raises exception, if fails.
+  def check(config)
+    check_jobs config[:jobs]
+    check_packages config[:packages]
+    check_dependencies(config[:jobs], config[:packages])    
+  end
+  
+  private
+  include JobChecks
+end
+
+class WeakConfigChecker
+  # Checks configuration file.
+  # Checks only jobs names.
+  def check(config)
+    config[:jobs].each { |job| check_job_name(job) }
+  end
+
+  private
+  include JobChecks
+end
+
+# Uploads job into server, using database client.
+# Returns true if success.
+def upload_job(server, db_client, packages, job)
+  begin
+    if db_client.exists_job?(job)
+      STDERR.puts "#{job[:name]} already exists"
+      return false
+    end
+    
+    db_client.insert_job(job)
+
+    if job.has_key?(:binary)
+      File.open(job[:binary], 'r') { |file| db_client.insert_binary(file.read, job_options) }
+    end
+    
+    job[:packages].each do |package|
+      SPM::build(packages[package])
+      File.open(package, 'r') { |file| db_client.insert_package(file.read, { :package_name => package }.merge(job)) }
+      FileUtils.rm(package)
+    end
     return true
+    
   rescue Exception => e
-    STDERR.puts e.message
+    STDERR.puts e
     STDERR.puts e.backtrace
     return false
   end  
@@ -80,38 +267,36 @@ begin
   options = parse_options(ARGV)
 rescue Exception => e
   STDERR.puts e
+  STDERR.puts e.backtrace
   exit -1
 end
 
-config = get_options_from_file(options[:config])
-config[:jobs].map! { |job| { :user => options[:user] }.merge(job.symbolize_keys_recursive!) }
-
-packages = {}
-(config[:packages] || []).each do |package|
-  package.symbolize_keys_recursive!
-  package[:data] ||= []
-  package[:data].each { |h| h.symbolize_keys_recursive! }
-  packages[package[:name]] = package
-end
-
-DRb.start_service
-server = DRbObject.new_with_uri("druby://#{options[:host]}:#{options[:port]}")
-db_client = get_db_client(server, options[:host])
-
 begin
+  config = ConfigUtils::read_config(options)
+  checker = (options[:action] == :up ? StrongConfigChecker : WeakConfigChecker).new
+  checker.check(config)
+  
+  packages = {}
+  config[:packages].each { |package| packages[package[:name]] = package }
+
+  DRb.start_service
+  server = DRbObject.new_with_uri("druby://#{options[:host]}:#{options[:port]}")
+  db_client = get_db_client(server, options[:host])
+
   case options[:action]
   when :up
-    config[:jobs].each do |job_options|
-      done = upload_job(server, db_client, packages, job_options) and server.add_job(job_options)
-      puts "for job '#{job_options[:name]}': " + (done ? "done" : "fail")
+    config[:jobs].each do |job|
+      done = upload_job(server, db_client, packages, job) and server.add_job(job)
+      puts "for job '#{job[:name]}': " + (done ? "done" : "fail")
     end
   when :down
-    config[:jobs].each do |job_options|
-      result = server.kill_job(job_options) ? "done" : "fail"
-      puts "for job '#{job_options[:name]}': " + result
+    config[:jobs].each do |job|
+      result = server.kill_job(job) ? "done" : "fail"
+      puts "for job '#{job[:name]}': " + result
     end
   end
 rescue Exception => e
-  STDERR.puts e.message
+  STDERR.puts e
   STDERR.puts e.backtrace
+  exit -1
 end
